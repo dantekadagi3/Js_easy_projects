16609a9c558a65e6836100c041ab58c4c4ea6598
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-05-15 07:55:45 -0700

    Modal Window

---
+++ ModalWindow/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const modal=document.querySelector('.modal')
+const overlay=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
