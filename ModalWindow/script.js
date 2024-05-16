e14870955571bad710e1909457eb8363b2719c43
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-05-16 11:24:30 -0700

    Dice Game

--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -1,5 +1,27 @@
 'use strict';
-const modal=document.querySelector('.modal')
-const overlay=document.querySelector('.close-modal');
-const btnsOpenModal=document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log("Button clicked");
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
